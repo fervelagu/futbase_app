5dacd6f71807cb6fbebc69ddbff5a204b861c0f1
fer <fervelagu@gmail.com>
2021-10-05 20:43:19 -0500

    add config for icons on android

--- android/app/src/main/java/com/futbase_app/MainActivity.java
+++ android/app/src/main/java/com/futbase_app/MainActivity.java
@@ -1,3 +1,4 @@
+import android.os.Bundle;
 package com.futbase_app;
 
 import com.facebook.react.ReactActivity;
@@ -8,6 +9,11 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "futbase_app";
